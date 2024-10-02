912cba7cfb2e503a7532783e1eeb0e31f3b71885
zamit00 <zamit00@gmail.com>
2024-10-02 16:35:20 +0300

    Add files via upload

---
+++ jvs.js
@@ -0,0 +1,9 @@
+document.getElementById('goToPage2')?.addEventListener('click', () => {
+    window.location.href = 'netunim.html';
+});
+
+document.getElementById('goToPage1')?.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+    
