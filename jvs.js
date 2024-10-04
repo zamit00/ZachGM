ea34219f9147ab94e9fe9f71cfd9b3b3df1e3336
zamit00 <zamit00@gmail.com>
2024-10-04 20:11:58 +0300

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -19,6 +19,7 @@ document.getElementById('mybutton').addEventListener('click', () => {
         }
     }
 // קורא נתונים מקובץ דאטה
+    if (kupaID==="") {return};
     fetch('data.txt')
         .then(response => response.text())
         .then(data => {
