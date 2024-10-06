ec18fc17d88a9d96fa051f5006872bf6034bfe33
zamit00 <zamit00@gmail.com>
2024-10-07 01:39:47 +0300

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -1,16 +1,8 @@
 
-document.getElementById('product').addEventListener('change', function() {
-     handleSelectChange();
-     handlemaslul();
-});
-document.getElementById('management-type').addEventListener('change', function() {
-     handleSelectChange();
-     handlemaslul();
-});
-
+document.getElementById('product').addEventListener('change', handleSelectChange);
+document.getElementById('management-type').addEventListener('change', handleSelectChange);
 document.getElementById('maslul-type').addEventListener('change', function() {
   maslulselect();
-  
 });
 
 document.querySelectorAll('input[type="radio"]').forEach(function(radio) {
@@ -18,39 +10,9 @@ document.querySelectorAll('input[type="radio"]').forEach(function(radio) {
 });
 
 // --------------------------------------------------------------------------------------------
-function handlemaslul() {
-
-let selectElement = document.getElementById('maslul-type');
-
-if (selectElement.options.length === 0) {
-
-    const myElement1 = document.getElementById('allthetables1');
-    myElement1.style.display = 'none';
-    const myElement2 = document.getElementById('allthetables2');
-    myElement2.style.display = 'none';
-    const myElement3 = document.getElementById('allthetables3');
-    myElement3.style.display = 'none';
-     
-  for (let i = 0; i<5;i++){
-  const maslult = document.getElementById('mas' + i);
-  const tsuabest = document.getElementById('tas' + i);
-    maslul.textContent='';
-    tsuabest.textContent='';
-
-  }
-} 
-
-
-
 function handleSelectChange() {
    
-for (let i = 0; i<5;i++){
-  const maslult = document.getElementById('mas' + i);
-  const tsuabest = document.getElementById('tas' + i);
-    maslul.textContent='';
-    tsuabest.textContent='';
-
-  }
+    
           const myElement1 = document.getElementById('allthetables1');
           myElement1.style.display = 'none';
           const myElement2 = document.getElementById('allthetables2');
@@ -356,4 +318,3 @@ if (!data.includes(searchString)) {
       
       
             
-
