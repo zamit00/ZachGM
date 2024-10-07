517c5a4e4391b2263bb41a292f994420142faa10
zamit00 <zamit00@gmail.com>
2024-10-07 15:22:39 +0300

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -288,6 +288,11 @@ if (!data.includes(searchString)) {
             document.getElementById('output5').textContent = fields[4] || '';
             document.getElementById('output6').textContent = Number(fields[6] || 0).toLocaleString() + " מלשח";
             document.getElementById('output7').textContent = fields[7] + '%';
+          
+            if (isNaN(fields[8])) {document.getElementById('output8').textContent="הוקם בתאירך : " + fields[46];
+            }
+            else{document.getElementById('output8').textContent = fields[8] + '%';}
+          
             document.getElementById('output8').textContent = fields[8] + '%';
             document.getElementById('output9').textContent = fields[9] + '%';
             document.getElementById('output10').textContent = fields[11];
