acb041e2e96a8191ff527f0152173698ceed2056
zamit00 <zamit00@gmail.com>
2024-10-13 10:26:51 +0300

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -102,8 +102,8 @@ function getMaslul(x, y) {
           fieldRashi.forEach(function(item) {
           let fields = item.split(',');              
            if (y===x){
-            if (fields[3] && fields[5] && fields[41] && fields[43] && fields[33] && fields[45] ){
-                  if (fields[3].includes(x) && item.includes(y) && fields[41]>= safDown1
+            if (fields[4] && fields[3] && fields[5] && fields[41] && fields[43] && fields[33] && fields[45] ){
+                  if (fields[4].includes("מגדל מקפת") && fields[3].includes(x) && item.includes(y) && fields[41]>= safDown1
                    && fields[41]< safUp1  && fields[43]>= safDown2 && fields[43]< safUp2  && fields[33]>= safDown3
                    && fields[33]< safUp3  && fields[45]>= safDown4 && fields[45]< safUp4) {
                   addOption(fields[1], fields[2],fields[9]);
@@ -113,7 +113,7 @@ function getMaslul(x, y) {
           if (y === "אקטיבי") {
     
             const actionsA = [act1, act2, act3, act4, act5, act6,act11];
-            if (fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
+            if (fields[4].includes("מגדל מקפת") && fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
                 if (fields[3].includes(x) && actionsA.some(action => fields[5].includes(action)) && fields[41]>= safDown1
                    && fields[41]< safUp1  && fields[43]>= safDown2 && fields[43]< safUp2  && fields[33]>= safDown3
                    && fields[33]< safUp3  && fields[45]>= safDown4 && fields[45]< safUp4) {
@@ -124,7 +124,7 @@ function getMaslul(x, y) {
 
           if (y==="פאסיבי"){ 
                const actionsP = [act7, act8];
-              if (fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
+              if (fields[4].includes("מגדל מקפת") && fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
                   if (fields[3].includes(x) && actionsP.some(action => fields[5].includes(action)) && fields[41]>= safDown1
                    && fields[41]< safUp1  && fields[43]>= safDown2 && fields[43]< safUp2  && fields[33]>= safDown3
                    && fields[33]< safUp3  && fields[45]>= safDown4 && fields[45]< safUp4) {
@@ -133,7 +133,7 @@ function getMaslul(x, y) {
                }
           }
           if (y==="קיימות"){
-              if (fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
+              if (fields[4].includes("מגדל מקפת") && fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
                   if (fields[3].includes(x) && fields[5].includes(act9) && fields[41]>= safDown1
                    && fields[41]< safUp1  && fields[43]>= safDown2 && fields[43]< safUp2  && fields[33]>= safDown3
                    && fields[33]< safUp3  && fields[45]>= safDown4 && fields[45]< safUp4 ) {
@@ -143,7 +143,7 @@ function getMaslul(x, y) {
           }
 
           if (y==="הלכתי"){
-              if (fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
+              if (fields[4].includes("מגדל מקפת") && fields[3] && fields[5] && fields[41] && fields[43] && fields[45] && fields[33]){
                   if (fields[3].includes(x) && fields[5].includes(act10) && fields[41]>= safDown1
                    && fields[41]< safUp1  && fields[43]>= safDown2 && fields[43]< safUp2  && fields[33]>= safDown3
                    && fields[33]< safUp3  && fields[45]>= safDown4 && fields[45]< safUp4) {
